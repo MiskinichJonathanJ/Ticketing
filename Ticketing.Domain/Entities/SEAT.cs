65d43969b9d91d27ec1853f777f9c4d11f61dc3d
marisolgit <marii.med110@gmail.com>
2026-04-18 11:03:57 -0300

    create entities Event, Sector, Seat

---
+++ Ticketing.Domain/Entities/SEAT.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ticketing.Domain.Entities
+{
+    public class SEAT
+    {
+        public Guid Id { get; set; }
+        public int SectorId { get; set; }
+        public string RowIdentifier { get; set; } = string.Empty;
+        public int SeatNumber { get; set; }
+        public string Status { get; set; } = "Available";
+        public int Version { get; set; } = 0;
+
+        public SECTOR Sector { get; set; } = null!;
+
+        // public RESERVATION? Reservation { get; set; }
+
+    }
+}
---
+++ Ticketing.Domain/Entities/SECTOR.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ticketing.Domain.Entities
+{
+    public class SECTOR
+    {
+        public int Id { get; set; }
+        public int EventId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public int Capacity { get; set; }
+
+        public EVENT Event { get; set; } = null!;
+
+        public ICollection<SEAT> Seats { get; set; } = [];
+    }
+}
