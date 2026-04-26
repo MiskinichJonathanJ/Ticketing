e1f6e2f573570d254272a2241235240741fd0132
MiskinichJonathanJ <miskinich.jobs.jonathan@gmail.com>
2026-04-26 18:27:05 -0300

    feat: Remove default WeatherForecast and add Event/Reservation controllers

---
+++ Ticketing/Controllers/EventController.cs
@@ -0,0 +1,43 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Ticketing.Application.DTOs;
+
+namespace Ticketing.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class EventController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public EventController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<EventDto>> GetAllEvents()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<EventDto> GetEventById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{id}/sectors")]
+        public async Task<IEnumerable<SectorDto>> GetEventSectors(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{id}/seats")]
+        public async Task<IEnumerable<SeatDto>> GetEventSeats(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Ticketing/Controllers/ReservationController.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Ticketing.Application.UseCases.Reservation.Commands;
+
+namespace Ticketing.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ReservationController(IMediator mediator) : Controller
+    {
+        private readonly IMediator _mediator = mediator;
+
+        [HttpPost]
+        public async Task<IActionResult> CreateReservation([FromBody] ReserveSeatCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+    }
+}
