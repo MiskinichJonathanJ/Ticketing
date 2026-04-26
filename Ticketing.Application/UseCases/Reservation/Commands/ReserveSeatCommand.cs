d0b1a055d57c38e1ecb8f3e087af7427d9f64df0
MiskinichJonathanJ <miskinich.jobs.jonathan@gmail.com>
2026-04-26 18:32:24 -0300

    feat(reservation): Define ReserveSeatCommand with SeatId and UserId properties

--- Ticketing.Application/UseCases/Reservation/Commands/ReserveSeatCommand.cs
+++ Ticketing.Application/UseCases/Reservation/Commands/ReserveSeatCommand.cs
@@ -1,6 +1,11 @@
-﻿namespace Ticketing.Application.UseCases.Reservation.Commands
+﻿using MediatR;
+using Ticketing.Application.DTOs;
+
+namespace Ticketing.Application.UseCases.Reservation.Commands
 {
-    public class ReserveSeatCommand
+    public class ReserveSeatCommand(Guid seatId, int userId) : IRequest<ReservationDto>
     {
+        public Guid SeatId { get; set; } = seatId;
+        public int UserId { get; set; } = userId;
     }
 }
