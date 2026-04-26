0c652930759a4e83e6c1128167364010ce88feec
MiskinichJonathanJ <miskinich.jobs.jonathan@gmail.com>
2026-04-26 18:34:16 -0300

    feat(infra): Introduce UnitOfWork for transactional consistency

---
+++ Ticketing.Infrastructure/Data/Repositories/UnitOfWork.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using Ticketing.Application.Interfaces;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly DbContext _context;
+    private IDbContextTransaction? _transaction;
+
+    public UnitOfWork(DbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task BeginTransactionAsync(CancellationToken cancellationToken)
+    {
+        _transaction = await _context.Database.BeginTransactionAsync(cancellationToken);
+    }
+
+    public async Task CommitAsync(CancellationToken cancellationToken)
+    {
+        await _context.SaveChangesAsync(cancellationToken);
+        await _transaction.CommitAsync(cancellationToken);
+    }
+
+    public async Task RollbackAsync(CancellationToken cancellationToken)
+    {
+        await _transaction.RollbackAsync(cancellationToken);
+    }
+}
