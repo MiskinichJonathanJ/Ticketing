a5dc48d22a1f6e717d045d03f7726ca749582428
MiskinichJonathanJ <miskinich.jobs.jonathan@gmail.com>
2026-04-26 15:37:39 -0300

    fix: arreglarr error entre handler y  query

--- Ticketing.Application/UseCases/Events/Queries/GetEventSeatsQuery.cs
+++ Ticketing.Application/UseCases/Events/Queries/GetEventSeatsQuery.cs
@@ -3,7 +3,7 @@ using Ticketing.Application.DTOs;
 
 namespace Ticketing.Application.UseCases.Events.Queries
 {
-    public class GetEventSeatsQuery : IRequest<EventDto> //Cambiar por otro el eventDto por  otro  con mas  sentido, pero por ahora lo dejo asi
+    public class GetEventSeatsQuery : IRequest<List<SeatDto>>
     {
     }
 }
