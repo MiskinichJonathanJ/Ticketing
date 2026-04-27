e5691a12236309888c186414abace98b497b344e
marisolgit <marii.med110@gmail.com>
2026-04-27 17:42:36 -0300

    feat: implement GetEventsHandler

--- Ticketing.Application/UseCases/Events/Handlers/GetEventsHandler.cs
+++ Ticketing.Application/UseCases/Events/Handlers/GetEventsHandler.cs
@@ -1,14 +1,30 @@
-﻿using MediatR;
+﻿using AutoMapper;
+using MediatR;
 using Ticketing.Application.DTOs;
+using Ticketing.Application.Interfaces;
 using Ticketing.Application.UseCases.Events.Queries;
 
 namespace Ticketing.Application.UseCases.Events.Handlers
 {
     public class GetEventsHandler : IRequestHandler<GetEventsQuery, List<EventDto>>
     {
-        public Task<List<EventDto>> Handle(GetEventsQuery request, CancellationToken cancellationToken)
+
+        private readonly IEventRepository _eventRepository;
+        private readonly IMapper _mapper;
+
+
+        public GetEventsHandler (IEventRepository eventRepository, IMapper mapper)
         {
-            throw new NotImplementedException();
+            _eventRepository = eventRepository;
+            _mapper = mapper;
+        }
+
+
+        public async Task<List<EventDto>> Handle(GetEventsQuery request, CancellationToken cancellationToken)
+        {
+            var events = await _eventRepository.GetAllAsync(); // trae todos los eventos de la DB
+
+            return _mapper.Map<List<EventDto>>(events); // convierte de entidad a DTO usando AutoMapper
         }
     }
 }
