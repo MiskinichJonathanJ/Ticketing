91b4944d2b23bac43db1cd460592147f4a804588
MiskinichJonathanJ <miskinich.jobs.jonathan@gmail.com>
2026-04-30 22:01:24 -0300

    feat: agregar inmutabilidad a los campos

--- Ticketing.Application/UseCases/Reservation/Commands/ReserveSeat/ReserveSeatCommand.cs
+++ Ticketing.Application/UseCases/Reservation/Commands/ReserveSeat/ReserveSeatCommand.cs
@@ -5,7 +5,7 @@ namespace Ticketing.Application.UseCases.Reservation.Commands.ReserveSeat
 {
     public class ReserveSeatCommand(Guid seatId, int userId) : IRequest<ReservationDto>
     {
-        public Guid SeatId { get; set; } = seatId;
-        public int UserId { get; set; } = userId;
+        public Guid SeatId { get; init; } = seatId;
+        public int UserId { get; init; } = userId;
     }
 }
