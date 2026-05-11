bad972a2ff70705c062b0eee1b9f98ed707eb417
marisolgit <marii.med110@gmail.com>
2026-05-11 11:32:27 -0300

    feat: add PaymentDto

---
+++ Ticketing.Application/DTOs/PaymentDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ticketing.Application.DTOs
+{
+    public class PaymentDto
+    {
+        public Guid ReservationId { get; set; }
+        public Guid SeatId { get; set; }
+        public string SeatStatus { get; set; } = string.Empty;
+        public string ReservationStatus { get; set; } = string.Empty;
+        public DateTime PaidAt { get; set; }
+        public string Message { get; set; } = string.Empty;
+    }
+}
